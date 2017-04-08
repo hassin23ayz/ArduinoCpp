0e50d03ad58b01fae47fb39c81ae8ded2a457a8b
ayz <hassin23ayz@gmail.com>
2017-04-09 01:57:49 +0600

    bit field and union example added

---
+++ T47_bit_field/T47_bit_field.ino
@@ -0,0 +1,30 @@
+namespace Ayz
+{
+  struct Date
+  {
+    unsigned int d: 5; // d has value between 1 and 31, so 5 bits
+    unsigned int m: 4; // m has value between 1 and 12, so 4 bits
+    unsigned int y;
+  };
+}
+
+using namespace Ayz;
+struct Date today = {31,12,2017};
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  unsigned int tmp = 9;
+  Serial.print("size of unsigned int type variable : ");
+  Serial.print(sizeof(tmp));
+  Serial.println("bytes");
+  Serial.print("size of custom Date variable : ");
+  Serial.print(sizeof(today));
+  Serial.println("bytes");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ T48_unionUseInRegister/T48_unionUseInRegister.ino
@@ -0,0 +1,45 @@
+typedef union
+{
+  struct
+  {
+    uint8_t  CAL_0:1;
+    uint8_t  CAL_1:1;
+    uint8_t  CAL_2:1;
+    uint8_t  CAL_3:1;
+    uint8_t  CAL_4:1;
+    uint8_t  CAL_5:1;
+    uint8_t  CAL_6:1;
+    uint8_t  CAL_7:1;
+  }b;              //distributed in bits
+  uint8_t u8;      //all in one 
+} REG_8Type;
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  REG_8Type myReg;
+  uint8_t tmp = OSCCAL; //Read the  Oscillator Calibration Register value 
+  myReg.u8 = tmp; //assign the register value to the union REG_8Type type to the u8 . 
+                      //And as union will hold only one member So the value put at u8 will get divided to the members of the struct b 
+
+  Serial.print("Original OSCCAL value in bits are :");
+  Serial.println(OSCCAL, BIN);
+  Serial.print("value of u8 in the union is :");
+  Serial.println(myReg.u8, BIN); 
+  
+  Serial.print("distributed values of struct b in the union are :");
+  Serial.print(myReg.b.CAL_7);
+  Serial.print(myReg.b.CAL_6);
+  Serial.print(myReg.b.CAL_5);
+  Serial.print(myReg.b.CAL_4);
+  Serial.print(myReg.b.CAL_3);
+  Serial.print(myReg.b.CAL_2);
+  Serial.print(myReg.b.CAL_1);
+  Serial.print(myReg.b.CAL_0);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
